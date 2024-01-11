be2e85479a43d0de0fed6b9c06ed91e95f87adf8
Gustav Westling <gustav@polar.sh>
2024-01-11 09:51:20 +0100

    Add react-email example

---
+++ src/app/email/route.tsx
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Container,
+  Head,
+  Html,
+  Row,
+  Section,
+} from "@react-email/components";
+import { render } from "@react-email/render";
+import { Tailwind } from "@react-email/tailwind";
+import { NextRequest, NextResponse } from "next/server";
+import { twConfig } from "../tw";
+
+export const runtime = "nodejs";
+
+
+const renderEmail = async (req: NextRequest): Promise<NextResponse> => {
+  const html = render(
+    <Html lang="en">
+      <Tailwind config={twConfig}>
+        <Head />
+        <Body className="font-sans">
+          <Container>
+            <Section>
+              <Row>
+                <Section className="bg-gray-100 p-4">Hello without media queries</Section>
+              </Row>
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>,
+    {
+      pretty: true,
+    }
+  );
+
+  return new NextResponse(html, {
+    status: 200,
+    headers: { "Content-Type": "text/html" },
+  });
+};
+
+export async function GET(req: NextRequest): Promise<NextResponse> {
+  return renderEmail(req);
+}
